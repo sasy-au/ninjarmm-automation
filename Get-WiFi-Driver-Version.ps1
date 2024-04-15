13c7b2807a2fd3d5381ffa636f73b18787de6ae7
david-sasy <96100062+david-sasy@users.noreply.github.com>
2024-04-16 09:14:12 +0930

    Update Get-WiFi-Driver-Version.ps1
    Added capture of adaptor name (ie: chipset)

--- Get-WiFi-Driver-Version.ps1
+++ Get-WiFi-Driver-Version.ps1
@@ -1,5 +1,8 @@
-## Gets the WLAN driver version and saves to a Ninja attribute
+## Gets the WLAN adaptor name and driver version, saves to Ninja attributes
+$drivername = ((netsh wlan show drivers) -Match '^\s+Driver' -Replace '^\s+Driver\s+:\s+','')
 $driverversion = ((netsh wlan show drivers) -Match '^\s+Version' -Replace '^\s+Version\s+:\s+','')
 
+Write-Host "Adaptor $drivername"
 Write-Host "Version $driverversion"
 Ninja-Property-Set wlanDriverVersion $driverversion
+Ninja-Property-Set wlanAdaptorName $drivername
